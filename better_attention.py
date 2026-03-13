e1efdcc05d540ed955b5dc2cbbb5e280deccb585
johnchennewyork-coder <johnchennewyork@gmail.com>
2026-03-12 20:14:33 -0400

    Implement Attention class with forward method
    Fixes the initialization of the Attention class and adds the forward method for attention calculation.

---
+++ better_attention.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.nn.Functional as F
+import math
+
+class Attention(nn.Module):
+  
+  def __init__(self, d_model, num_heads):  
+    self.d_model = d_model
+    self.d_key = self.d_model//num_heads
+    
+    self.W_q = nn.Linear(self.d_model, self.d_model)
+    self.W_k = nn.Linear(self.d_model, self.d_model)
+    self.W_v = nn.Linear(self.d_model, self.d_model)
+    self.W_o = nn.Linear(self.d_model, self.d_model)
+    
+  def forward(self, x):
+    Q = self.W_q(x)
+    K = self.W_k(x)
+    V = self.W_v(x) # weight matrix + bias term 
+    
+    attn_logits = Q @ K.transpose(-2,-1) / math.sqrt(self.d_key)
+    attn_weights = F.softmax(attn_logits, dim=-1) 
+    context_vector = attn_weight * V
+    
+    return context_vector
+    
