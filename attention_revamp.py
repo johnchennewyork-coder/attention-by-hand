5c3d64497bab865e25d5dfd60d35b426ce120992
johnchennewyork-coder <johnchennewyork@gmail.com>
2026-04-05 16:35:10 -0400

    Fix attention mechanism variable references

--- attention_revamp.py
+++ attention_revamp.py
@@ -3,6 +3,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 class Attention(nn.Module):
   def __init__(self, d_model, num_heads, causal_mask=False):
@@ -19,16 +20,16 @@ class Attention(nn.Module):
     BS, T_q, d_model = x.shape
 
     QKV = self.W_qkv(x)
-    Q, K, V = QKV.split(3, dim=2)
+    Q, K, V = QKV.split(self.d_model, dim=2)
 
     # transpose, reshaping
     mha_q = Q.reshape(BS, T_q, self.num_heads, self.d_key).transpose(1,2)
     mha_k = K.reshape(BS, T_q, self.num_heads, self.d_key).transpose(1,2)
     mha_v = V.reshape(BS, T_q, self.num_heads, self.d_key).transpose(1,2)
 
-    attn_logits = mha_q @ mha_k.transpose(-2,-1)/math.sqrt(d_key) 
+    attn_logits = mha_q @ mha_k.transpose(-2,-1)/math.sqrt(self.d_key) 
     attn_weights = F.softmax(attn_logits, dim=-1)
-    context_vector = attn_weights @ mha_V # 
+    context_vector = attn_weights @ mha_v # 
     concatted_vec = context_vector.transpose(1,2).contiguous().view(BS, T_q, self.d_model)
     output_vec  = self.W_o(concatted_vec)
     return output_vec 
