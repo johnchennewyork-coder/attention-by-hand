79e98a1cf23bc18efa9a790197b033ca480e6f2b
johnchennewyork-coder <johnchennewyork@gmail.com>
2026-04-11 18:22:35 -0400

    Refactor RNN class and add BasicRNN implementation

--- rnn.py
+++ rnn.py
@@ -1,16 +1,36 @@
 
-
+import numpy as np
 import torch
 import torch.nn as nn 
 import torch.nn.functional as F
 
-class RNN(nn.Module):
+class RegularNN(nn.Module):
   def __init__(self,input_size, hidden_size, output_size):
+    super(RegularNN, self).__init__()
     self.input = nn.Linear(input_size, hidden_size)
     self.h1 = nn.Linear(hidden_size, output_size)
     
-def forward(self, x):
-  intermediate = self.input(x)
-  out = nn.ReLu(self.h1(intermediate))
-  return out 
+  def forward(self, x):
+    intermediate = self.input(x)
+    out = F.ReLU(self.h1(intermediate))
+    return out 
+
+class BasicRNN(nn.Module):
+  def __init__(self,input_size, hidden_size, output_size):
+    super(BasicRNN, self).__init__()
+    self.i2h = nn.Linear(input_size, hidden_size)
+    self.h2o = nn.Linear(hidden_size, output_size)
     
+  def forward(self, x):
+    hidden = self.i2h(x)
+    out = self.h2o(F.ReLU(hidden))
+    return out 
+
+def generate_train_data():
+  vals = torch.randn()
+  return vals 
+
+if __name__ == '__main__':
+  print('alright')
+  
+  
