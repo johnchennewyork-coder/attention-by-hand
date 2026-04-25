b8357a5b9c827723b6c79ee040af39c5584efb09
johnchennewyork-coder <johnchennewyork@gmail.com>
2026-04-25 14:54:58 -0400

    Replace LSTM with RNN in RNN class

--- rnns_again.py
+++ rnns_again.py
@@ -7,11 +7,12 @@ import torch.nn.functional as F
 
 class RNN(nn.Module):
   def __init__(self, input_size, hidden_size, output_size):
-    self.i2h = nn.Linear(input_size, hidden_size)
-    self.h2o = nn.LSTM(hidden_size, output_size)
+    super().__init__()
+    self.i2h = nn.RNN(input_size, hidden_size)
+    self.h2o = nn.Linear(hidden_size, output_size)
 
   def forward(self, x):
-    intermediate = self.i2h(x)
-    hidden, cell_tape = self.h2o(intermediate)
-    return hidden
+    output, hidden = self.i2h(x)
+    out = self.h2o(output)
+    return out
 
