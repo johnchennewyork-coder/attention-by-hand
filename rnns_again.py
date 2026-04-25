26d5724f39928a9fd2e698a805476162582ae69c
johnchennewyork-coder <johnchennewyork@gmail.com>
2026-04-25 14:57:28 -0400

    Update rnns_again.py

--- rnns_again.py
+++ rnns_again.py
@@ -8,11 +8,11 @@ import torch.nn.functional as F
 class RNN(nn.Module):
   def __init__(self, input_size, hidden_size, output_size):
     super().__init__()
-    self.i2h = nn.RNN(input_size, hidden_size)
+    self.i2h = nn.RNN(input_size, hidden_size, batch_first=True) # transformer semantics 
     self.h2o = nn.Linear(hidden_size, output_size)
 
   def forward(self, x):
-    output, hidden = self.i2h(x)
+    output, hidden = self.i2h(x) # note that RNN returns a sequence of length K 
     out = self.h2o(output)
     return out
 
